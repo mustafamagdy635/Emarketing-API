761ab933af58ee19514cde967d8a6efe9c3b81c7
mustafa magdy <mustafamagdy635@gmail.com>
2024-08-03 23:58:14 +0300

    nnn

--- Emarketing API.Utility/SD.cs
+++ Emarketing API.Utility/SD.cs
@@ -23,5 +23,6 @@ namespace Emarketing_API.Utility
         public const string PaymentStatusApproved = "Approved";
         public const string PaymentStatusDelayedPayment = "ApprovedForDelayedPayment";
         public const string PaymentStatusRejected = "Rejected";
+        public const string PaymentStatusRejectedw = "dsdsds";
     }
 }
